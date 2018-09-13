1da717727058958b0ac38f3fa85e01c6d4634ef3
ThomasGauthierr <thomasg70360@gmail.com>
2018-09-13 08:06:50 +0200

    UrlMappings fixed

--- grails-app/controllers/fr/mbds/firstgrails/UrlMappings.groovy
+++ grails-app/controllers/fr/mbds/firstgrails/UrlMappings.groovy
@@ -9,8 +9,7 @@ class UrlMappings {
             }
         }
 
-//        "/"(view:"/index")
-        "/"(controller:'User', action:'index')
+        "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
