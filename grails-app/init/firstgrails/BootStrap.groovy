eef04a416983e46a54d275e74789097a99a9d10d
ThomasGauthierr <thomasg70360@gmail.com>
2018-09-13 08:45:13 +0200

    UserRoles added to the bootstrap

--- grails-app/init/firstgrails/BootStrap.groovy
+++ grails-app/init/firstgrails/BootStrap.groovy
@@ -6,6 +6,18 @@ class BootStrap {
 
     def init = { servletContext ->
 
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        def gamingRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+        def adminUser = new User(username: 'admin', password: 'password').save(flush: true)
+        def playerUser = new User(username: 'player', password: 'password').save(flush: true)
+        def playerTwoUser = new User(username: 'playerTwo', password: 'password').save(flush: true)
+
+        UserRole.create(adminUser, adminRole,true)
+        UserRole.create(playerUser, gamingRole, true)
+        UserRole.create(playerTwoUser, gamingRole, true)
+
+
 //        def user1 = new User(username: 'user1').save(flush: true, failOnError: true)
 //        def user2 = new User(username: 'user2').save(flush: true, failOnError: true)
 //
