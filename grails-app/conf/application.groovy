8de2e5e710c295c73b70f93676939d707eb240f8
Amine <aiterrami.amine10@gmail.com>
2018-09-14 13:19:49 -0700

    Login page as default page

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -19,7 +19,7 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 
 grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/assets/**',      filters: 'none'],
-	[pattern: '/**/js/**',       filters: 'none'],
+		[pattern: '/**/js/**',       filters: 'none'],
 	[pattern: '/**/css/**',      filters: 'none'],
 	[pattern: '/**/images/**',   filters: 'none'],
 	[pattern: '/**/favicon.ico', filters: 'none'],
