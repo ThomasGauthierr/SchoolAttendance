5dd8e1e9206997c15a655b2246741453c2cf7267
Unknown <a4ramy@hotmail.com>
2018-09-17 19:35:23 +0200

    Display matches in another table in the index

--- grails-app/controllers/fr/mbds/firstgrails/MainController.groovy
+++ grails-app/controllers/fr/mbds/firstgrails/MainController.groovy
@@ -11,6 +11,7 @@ class MainController {
 
     def springSecurityService = Holders.applicationContext.springSecurityService
     def userService
+    def matchService
 
     def index() {
         def user = User.get(springSecurityService.currentUser.id)
@@ -18,7 +19,7 @@ class MainController {
         for(def authority : springSecurityService.getPrincipal().getAuthorities()) {
             if(authority.getAuthority() == 'ROLE_ADMIN') {
                 params.max = Math.min(10 ?: 10, 100)
-                respond userService.list(params), view: '/index-admin', model:[user: user, userCount: userService.count()]
+                respond userService.list(params), view: '/index-admin', model:[user: user, userCount: userService.count(), matches: matchService.list(params)]
                 return
             }
         }
