3fc569c2836e7efa43f9691b30b29993822cc310
Amine <a4ramy@hotmail.com>
2018-09-18 16:59:45 -0700

    Route

--- grails-app/controllers/fr/mbds/firstgrails/ApiController.groovy
+++ grails-app/controllers/fr/mbds/firstgrails/ApiController.groovy
@@ -1,6 +1,9 @@
 package fr.mbds.firstgrails
 
+import grails.converters.JSON
+
 class ApiController {
+     def userService
 
     def index() {
         switch (request.getMethod()) {
@@ -16,4 +19,26 @@ class ApiController {
                 break
         }
     }
+
+    def users(long id) {
+        switch (request.getMethod()) {
+            case "GET" :
+                if(id != 0) {
+                    render userService.get(id) as JSON
+                } else {
+                    render userService.list() as JSON
+                }
+                break
+
+            case "POS":
+
+                def user = new User();
+                user.username = params.user.username;
+                user.password = params.user.password
+
+                userService.save(user)
+
+        }
+    }
+
 }
