83689533949c8271feb24aa0de13d78c3f0bae78
Amine <a4ramy@hotmail.com>
2018-10-04 17:01:51 +0200

    Ok  for spring secrurity rest config

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -4,6 +4,10 @@ grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'fr.mbds.firstg
 grails.plugin.springsecurity.authority.className = 'fr.mbds.firstgrails.Role'
 grails.plugin.springsecurity.requestMap.className = 'fr.mbds.firstgrails.UserRole'
 grails.plugin.springsecurity.securityConfigType = 'Annotation'
+grails.plugin.springsecurity.rest.token.storage.jwt.useEncryptedJwt = true
+
+grails.plugin.springsecurity.conf.rest.token.storage.jwt.secret = 'qrD6h8K6S9503Q06Y6Rfk21TErImPYqa'
+
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/**/main/index',               access: ['ROLE_ADMIN', 'ROLE_USER']],
 	[pattern: '/**/main/',               access: ['ROLE_ADMIN', 'ROLE_USER']],
@@ -34,13 +38,13 @@ grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/**/css/**',      filters: 'none'],
 	[pattern: '/**/images/**',   filters: 'none'],
 	[pattern: '/**/favicon.ico', filters: 'none'],
-	[pattern: '/**',             filters: 'JOINED_FILTERS']
+	[pattern: '/**',             filters: 'JOINED_FILTERS'],
+	[pattern: '/api/**',          filters: 'JOINED_FILTERS,-anonymousAuthenticationFilter,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter,-rememberMeAuthenticationFilter']
 ]
 
 grails.plugin.springsecurity.successHandler.defaultTargetUrl = '/main/connection'
 
 //Enable logout through GET
 grails.plugin.springsecurity.logout.postOnly = false
-
 grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*', '/fonts/*']
 grails.resources.adhoc.includes = ['/images/**', '/css/**', '/js/**', '/plugins/**', '/fonts/**']
